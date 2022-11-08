9f5ab237dc0e154075a3edc6088f917ea1912a6b
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-11-08 20:44:39 +0530

    first commit

---
+++ src/main/java/com/naveen/springbootdeepdive/SpringbootDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.springbootdeepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeepDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naveen/springbootdeepdive/SpringbootDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.springbootdeepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeepDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
